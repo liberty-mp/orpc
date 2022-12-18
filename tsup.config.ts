76518a84c0053544fb8780c95da06bda256528d2
leonardssh <leonardssh22@gmail.com>
2022-12-18 14:46:20 +0100

    esm support

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	clean: true,
 	dts: true,
 	entry: ['src/**/*.ts'],
-	format: ['cjs'],
+	format: ['cjs', 'esm'],
 	minify: false,
 	skipNodeModulesBundle: true,
 	sourcemap: true,
